16658fd0497e54e3161c89705aaa6aea6ba1d864
liaqat-paindah <liaqat.paindah@email.com>
2026-08-26 14:17:10 +0430

    feat/zustand insalled

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button className="rounded-sm bg-blue-700 text-gray-50 hover:bg-blue-600">
+        {" "}
+        Click to Login
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
